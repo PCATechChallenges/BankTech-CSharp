fece8a4a9d82b1efbe1a0ac0b798dc29609c35cc
Derek Hampton <derek.hampton@augmensis.co.uk>
2015-04-22 19:44:24 +0100

    Added CallCenterSpam solution

--- 1-CallCentreSpam/Program.cs
+++ 1-CallCentreSpam/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace _1_CallCentreSpam
     {
         static void Main(string[] args)
         {
+            var phoneList = "PhoneList.txt";
+            var numbers = File.ReadLines(phoneList);
+            var spamCount = 0;
+            var goodCount = 0;
+
+            foreach (var phoneNumber in numbers)
+            {
+                var isSpam = phoneNumber.Contains("SPAM");
+                if (isSpam)
+                {
+                    spamCount++;
+                }
+                else
+                {
+                    goodCount++;
+                }
+
+                Console.WriteLine(isSpam ? "SPAM" : "OK");
+            }
+            Console.WriteLine("\n\n{2} numbers processed\n{0} SPAM numbers found\n{1} numbers were OK", spamCount, goodCount, spamCount + goodCount);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
